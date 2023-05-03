f43064d91db6f48fe7563f375176628767695b4f
thoph <thoph@dev.ndex.net>
2023-05-03 13:40:24 +0700

    flowermoundnailspa.com

--- Controllers/BaseController.cs
+++ Controllers/BaseController.cs
@@ -1,19 +1,35 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebNails.Models;
 
 namespace WebNails.Controllers
 {
     public class BaseController : Controller
     {
-        private string HyperLinkTell = "tel:(972)3556701";
-        private string TextTell = "(972)-355-6701";
         public BaseController()
         {
-            ViewBag.HyperLinkTell = HyperLinkTell;
-            ViewBag.TextTell = TextTell;
+            var jsonText = System.IO.File.ReadAllText(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data/info.json"));
+            var dataInfo = JsonConvert.DeserializeObject<InfoModel>(jsonText);
+            ViewBag.HyperLinkTell = dataInfo.HyperLinkTell;
+            ViewBag.TextTell = dataInfo.TextTell;
+            ViewBag.Name = dataInfo.Name;
+            ViewBag.Address = dataInfo.Address;
+            ViewBag.GooglePlus = dataInfo.GooglePlus;
+            ViewBag.LinkGoogleMapAddress = dataInfo.LinkGoogleMapAddress;
+            ViewBag.LinkIFrameGoogleMap = dataInfo.LinkIFrameGoogleMap ;
+
+            var txtBusinessHours = System.IO.File.ReadAllText(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data/business-hours.txt"));
+            ViewBag.BusinessHours = txtBusinessHours;
+
+            var txtHomeAboutUs = System.IO.File.ReadAllText(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data/home-about-us.txt"));
+            ViewBag.HomeAboutUs = txtHomeAboutUs;
+
+            var txtAboutUs = System.IO.File.ReadAllText(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data/about-us.txt"));
+            ViewBag.AboutUs = txtAboutUs;
         }
     }
 }
---
+++ Models/InfoModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebNails.Models
+{
+    public class InfoModel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string TextTell { get; set; }
+        public string HyperLinkTell { get; set; }
+        public string GooglePlus { get; set; }
+        public string LinkGoogleMapAddress { get; set; }
+        public string LinkIFrameGoogleMap { get; set; }
+        public string BusinessHours { get; set; }
+    }
+}
